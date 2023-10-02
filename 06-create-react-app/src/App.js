e1a69a71704b6d95d01383633f776a47ec49fe81
Darchiashvili9 <gdarchiashvili@hotmail.com>
2023-10-02 13:10:45 +0400

    1

--- 06-create-react-app/src/App.js
+++ 06-create-react-app/src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <h1>Hello crom Create React App</h1>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reloaddddddddddd.
         </p>
         <a
           className="App-link"
