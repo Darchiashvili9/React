0ed101bd0bb6fd3d22237bbfda337ea96f7773e4
Darchiashvili9 <gdarchiashvili@hotmail.com>
2023-10-03 17:04:16 +0400

    08

---
+++ 08-reusable-components/src/components/MyComponent.js
@@ -0,0 +1,9 @@
+function MyComponent() {
+  return (
+    <div>
+      <h1>hello from the reusable component</h1>
+      <button>Like!</button>
+    </div>
+  );
+}
+export default MyComponent;
---
+++ 08-reusable-components/src/components/OtherComponent.js
@@ -0,0 +1,9 @@
+function OtherComponent() {
+  return (
+    <div>
+      <p>hello from jsx component</p>
+    </div>
+  );
+}
+
+export default OtherComponent;
