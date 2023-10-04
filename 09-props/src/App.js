d0c8698ce002cce459dbe7ce2272bc52cc2b9a98
George Darchiashvili <Gdarchiashvili@hotmail.com>
2023-10-04 16:00:05 +0400

    09

--- 09-props/src/App.js
+++ 09-props/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import PetInfo from "./components/PetInfo";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <PetInfo animal="rat" age={1}/>
+    </div>
+  );
 }
 
 export default App;
--- 09-props/src/components/PetInfo.js
+++ 09-props/src/components/PetInfo.js
@@ -1,3 +1,15 @@
-function PetInfo() {
-  return <h1>Elene 4</h1>;
+function PetInfo(props) {
+  console.log(props);
+
+  //destruqturizacia
+  const { animal, age } = props;
+
+  return (
+    <h1>
+      Pentosa {animal} run away from natakhtari,age {age}
+    </h1>
+  );
 }
+
+export default PetInfo;
+ 
