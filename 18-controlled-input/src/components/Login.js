1e52a8ce491fd0e818b58d053697a8f44b646db7
Darchiashvili9 <gdarchiashvili@hotmail.com>
2023-10-10 16:39:57 +0400

    18

---
+++ 18-controlled-input/src/components/Login.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+function Login() {
+  // const [userName, setUserName] = useState("");
+  // const [password, setPassword] = useState("");
+
+  const [data, setData] = useState({ userName: "", password: "" });
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+
+    console.log(data);
+    alert(JSON.stringify(data));
+  }
+
+  function handleInputChange(text, name) {
+    setData({ ...data, [name]: text.target.value });
+  }
+
+  return (
+    <>
+      <h1>Login Form</h1>
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={data.userName}
+            onChange={(e) => handleInputChange(e, "userName")}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={data.password}
+            onChange={(e) => handleInputChange(e, "password")}
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+}
+
+export default Login;
