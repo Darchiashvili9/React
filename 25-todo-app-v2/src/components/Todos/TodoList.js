d4d2e9c08218cf3c600d43201670fc8fcc15ad9c
Darchiashvili9 <gdarchiashvili@hotmail.com>
2023-10-31 17:54:04 +0400

    25

--- 25-todo-app-v2/src/components/Todos/TodoList.js
+++ 25-todo-app-v2/src/components/Todos/TodoList.js
@@ -9,15 +9,8 @@ function TodoList({ todos, deleteTodo }) {
       {!todos.length ? (
         <h3>list is empty, add something</h3>
       ) : (
-        todos.map((todo, index) => {
-          return (
-            <Todo
-              key={index}
-              todo={todo}
-              deleteTodo={deleteTodo}
-              index={index}
-            />
-          );
+        todos.map((todo) => {
+          return <Todo key={todo.id} todo={todo} deleteTodo={deleteTodo} />;
         })
       )}
     </div>
